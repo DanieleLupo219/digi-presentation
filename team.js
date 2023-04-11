c5a10fa71e71c228d397601fa9b91cdf601726db
Edoardo <edoardoricca98@gmail.com>
2023-04-11 15:13:25 +0200

    eddo added

--- team.js
+++ team.js
@@ -1,1 +1,11 @@
-let team = [];
+let team = [{name: "Edoardo",
+             surname: "Ricca",
+             age: 24,
+             city: "Roma",
+             hobby: "Trekking",
+             favoriteFood: "Squid",
+             favoriteVideoGame: "",
+             favoriteFilm: "Akira",
+             favoriteBook: "Siddartha",
+             namePet: "Sloth"
+            }];
