92d79b174a2d034510abcb998a6244abd2f219a2
Luca Lai <lucaloi1889@gmail.com>
2023-04-11 14:21:14 +0200

    aggiunto Branch Luca

--- team.js
+++ team.js
@@ -1,1 +1,11 @@
-let team = [];
+let team = [{name: "Luca",
+             surname: "Lai",
+             age: 34,
+             city: "Cagliari",
+             hobby: "Calistenic",
+             favoriteFood: "Lasagne",
+             favoriteVideoGame: "Call of duty",
+             favoriteFilm: "",
+             favoriteBook: "",
+             petName: ""
+            }];;
