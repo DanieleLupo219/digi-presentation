5536b5f3642087ec820775d8369ec59a061ca69b
Daniele <danielelupo219@gmail.com>
2023-04-11 15:11:49 +0200

    Modified team.js

--- team.js
+++ team.js
@@ -1,1 +1,11 @@
-let team = [];
+let team = [{name: "Daniele",
+             surname: "Lupo",
+             age: 23,
+             city: "Palermo",
+             hobby: "Dancing",
+             favoriteFood: "Curry",
+             favoriteVideoGame: "Death Stranding",
+             favoriteFilm: "",
+             favoriteBook: "Demian by Hermann Hesse",
+             namePet: "Yupi"
+            }];
