aace36ac3a0c5d06df83a39da32ea13140d06205
Fernando Lanna <lanna.fernando@gmail.com>
2023-04-11 15:13:20 +0200

    aggiunto branch Fernando

--- team.js
+++ team.js
@@ -1,1 +1,11 @@
-let team = [];
+let team = [{name: "Fernando",
+             surname: "Lanna",
+             age: 27,
+             city: "Roma",
+             hobby: "Boxe",
+             favoriteFood: "Carbonara",
+             favoriteVideoGame: "Call of duty",
+             favoriteFilm: "Spider man",
+             favoriteBook: "fahrenheit 451",
+             namePet: ""
+            }];
